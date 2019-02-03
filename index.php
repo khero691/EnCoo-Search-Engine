6ab57fc4780f09ca31065eff544193bc56a40011
Vladyslav Halimskyi <30364795+khero691@users.noreply.github.com>
2019-02-03 19:33:25 +0100

    EXAMPLE
    This file is example for you!

---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<?
+
+ini_set('date.timezone', 'Europe/Warsaw');
+error_reporting(E_ALL);
+define('EC_DEBUG', 1);
+
+include 'encoo.database.php';
+include 'encoo.searchengine.php';
+
+$cogs = array('tables' => array('ec_users'), 'fields' => array(array('id', 'name')));
+
+$SE = new EnCooSE(json_encode($cogs));
+
+if(isset($_GET['query']) && strlen($_GET['query'])) {
+
+	$arr = $SE->sortArray( $SE->search($_GET['query']), 'id' );
+
+	echo "<pre>";
+	print_r(json_encode($arr));
+
+	echo "</pre>";
+}
