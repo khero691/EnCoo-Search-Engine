9198d0661922bc6638751da09dc4bd3f4477df47
Vlad Mazikov <30364795+khero691@users.noreply.github.com>
2019-02-03 19:25:23 +0100

    Add files via upload

---
+++ encoo.database.php
@@ -0,0 +1,24 @@
+﻿<?
+
+/**
+* @author enCoo Developments © Vladyslav Halimskyi 2018
+* @package enCoo/SearchEngine/DataBaseConnect
+*/
+
+class DatabaseSettings {
+	var $settings;
+	function getSettings()
+	{
+		// Database variables
+		// Host name
+		$settings['dbhost'] = 'localhost';
+		// Database name
+		$settings['dbname'] = 'db_workbench';
+		// Username
+		$settings['dbusername'] = 'root';
+		// Password
+		$settings['dbpassword'] = '';
+		
+		return $settings;
+	}
+}
